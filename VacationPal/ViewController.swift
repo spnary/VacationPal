ea3c8f0850a7d8ea796ac5333e76c70713ac6714
Stephen Nary <spnary@gmail.com>
2019-03-03 15:39:47 -0500

    Initial Commit

---
+++ VacationPal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VacationPal
+//
+//  Created by Stephen Nary on 3/3/19.
+//  Copyright © 2019 Stephen Nary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
