d6dbcb3e58e67d81a8bb8e12fe60168385205b42
Stephen Nary <spnary@gmail.com>
2019-04-02 21:47:53 -0400

    set up basic storyboard flow

---
+++ VacationPal/ResortStayDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResortStayDetailViewController.swift
+//  VacationPal
+//
+//  Created by Stephen Nary on 4/2/19.
+//  Copyright © 2019 Stephen Nary. All rights reserved.
+//
+
+import UIKit
+
+class ResortStayDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
